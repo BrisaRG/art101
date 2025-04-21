8d0e25a5673c6d7b040e727b096bfee83a62dc56
BrisaRG <brisa.ruby22@gmail.com>
2025-04-21 16:40:23 -0700

    lab summary

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Pseudo code for lab 4
+// Author: Brisa Garcia
+// Date: April 21 2025
 
 // Constants
 
@@ -11,6 +11,17 @@ function myFunction(param1, param2) {
   // some code here
   // return results;
 }
+// Task: Rock-paper-scissors
+ // Put fist on hand
+  // Chant "Rock, paper, scissors, shoot," and play hand on "shoot"
+     // Rock hand beats scissors
+     // Scissors hand beats paper
+     // Paper hand beats rock
+// Hands must be played simultaneously with opponent
+// Evaluate played hands
+     // Win condition: hand beats opponent’s hand
+     // Draw condition: hand is the same as opponent’s
+       // Play again
 
 function main() {
   console.log("Main function started.");
