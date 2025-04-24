cb98361d52f6fdb119ee698ea9774a0837c74bfe
BrisaRG <brisa.ruby22@gmail.com>
2025-04-24 01:06:08 -0700

    lab5 art101

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - lab5datatypes&variables
+// Author: Brisa Garcia
+// Date:23 April 2025
 
 // Constants
+make = "Ford";
+model = "Focus";
+color = "Silver";
+year = 2013;
 
-// Functions
+// Calculate the age of the car
+age = 2025 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
